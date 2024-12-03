4b2c947e495b4abf0a58e6419216412b84e185a8
Hugo Quezada <hquezada@leomail.tamuc.edu>
2024-12-03 12:37:55 -0600

    add JS and modified HTML
    add event listener on buttons and display target in console. Replace input box with a <p> tag. Use flex display to place text on the right

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
 const input = document.querySelector('#input-box');
 const submit = document.querySelector('#submit');
-const digits = document.querySelector(".digits");
+const buttons = document.querySelector(".buttons");
+// const digits = document.querySelector(".digits");
+
+buttons.addEventListener("click", (e) => {
+    let target = e.target;
+
+    switch(target.id){
+        case "clear":
+            console.log("AC was clicked");
+            break;
+        case "par":
+            console.log("() was clicked");
+            break;
+        case "mod":
+            console.log("% was clicked");
+            break;
+        case "divide":
+            console.log("/ was clicked");
+            break;
+        case "seven":
+            console.log("7 was clicked");
+            break;
+        case "eight":
+            console.log("8 was clicked");
+            break;
+        case "nine":
+            console.log("9 was clicked");
+            break;
+        case "times":
+            console.log("* was clicked");
+            break;
+        case "four":
+            console.log("4 was clicked");
+            break;
+        case "five":
+            console.log("5 was clicked");
+            break;
+        case "six":
+            console.log("6 was clicked");
+            break;
+        case "plus":
+            console.log("+ was clicked");
+            break;
+    }
+});
+
 
 
